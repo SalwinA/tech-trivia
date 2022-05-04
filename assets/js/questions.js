7ed7bb68ae97928ac232fb82226acb589d9c2f69
SalwinA <salwino@gmail.com>
2022-05-04 10:25:08 +0000

    Add basic html structure and question list.

---
+++ assets/js/questions.js
@@ -0,0 +1,122 @@
+const question_bank =[
+    {
+        id: 1,
+        question: 'The Firefox browser logo is not a fox? It is actually a type of species which are protected in Asia.',
+        options: ['Slow Loris','Red Panda','Giant Panda'],
+        correctAnswer: 'Red Panda'
+    },
+    {
+        id: 2,
+        question: 'The first apple logo was featured by the famous physicist sitting under an apple tree?',
+        options: ['Galileo Galilei','Isaac Newton','Albert Einstein'],
+        correctAnswer: 'Isaac Newton'
+    },
+    {
+        id: 3,
+        question: 'The average computer user blinks only _____ times per minute, while the normal rate is 20 times per minute.',
+        options: ['7','2','10'],
+        correctAnswer: '7'
+    },
+    {
+        id: 4,
+        question: 'The “Google” name was created mistakenly. The original name was supposedly.,',
+        options: ['Googel','Googolplex','Googol'],
+        correctAnswer: 'Googol'
+    },
+    {
+        id: 5,
+        question: 'This font from Microsoft was a controversial font after the 9/11 attacks in 2001 where the first plane was first to hit.',
+        options: ['Arial Narrow','Lucida Console','Wingdings'],
+        correctAnswer: 'Wingdings'
+    },
+    {
+        id: 6,
+        question: 'The first VCR or Video Camera Recorder in the year 1956 was the size of a:',
+        options: ['Piano','Television','Radio'],
+        correctAnswer: 'Piano'
+    },
+    {
+        id: 7,
+        question: 'That _____ of people try to plug their USB devices upside down?',
+        options: ['86%','50%','30%'],
+        correctAnswer: '86%'
+    },
+    {
+        id: 8,
+        question: 'The first American alarm clock that was invented could only ring at ____?',
+        options: ['4am','6am','12pm'],
+        correctAnswer: '4am'
+    },
+    {
+        id: 9,
+        question: 'The first world computer mouse was made of:',
+        options: ['Metal','Wood','Chrome'],
+        correctAnswer: 'Wood'
+    },
+    {
+        id: 10,
+        question: 'The Astronauts of Apollo 11 couldn’t get life insurance because it cost a fortune? Instead, they signed:',
+        options: ['Autographs','Waiver','Farewell Letter'],
+        correctAnswer: 'Autographs'
+    },
+    {
+        id: 11,
+        question: 'What was the app Snapchat originally named when launched in 2011?',
+        options: ['Facesnap','Picaboo','Chatsnap'],
+        correctAnswer: 'Picaboo'
+    },
+    {
+        id: 12,
+        question: '_____ of people type their word or sentence on google, just to see if they spelled it right?',
+        options: ['10%','50%','97%'],
+        correctAnswer: '97%'
+    },
+    {
+        id: 13,
+        question: 'What does the word “Android” mean?',
+        options: ['A green robot.','A robot with human appearance.','An articulated robot.'],
+        correctAnswer: 'A robot with human appearance.'
+    },
+    {
+        id: 14,
+        question: 'What is the most used password despite all warnings is?',
+        options: ['123456','abcdef','abc123'],
+        correctAnswer: '123456'
+    },
+    {
+        id: 15,
+        question: 'Name the syndrome in which one thinks that their phone is vibrating but isn’t?',
+        options: ['Forced Vibration','Phantom Vibration','Damped Vibration'],
+        correctAnswer: 'Phantom Vibration'
+    },
+    {
+        id: 16,
+        question: 'Fear of technology is called ________.',
+        options: ['Glossophobia','Tripophobia','Technophobia'],
+        correctAnswer: 'Technophobia'
+    },
+    {
+        id: 17,
+        question: 'What is the first name of the famous character in video game Super Mario?',
+        options: ['Jumperman','Superjump','Jumpman'],
+        correctAnswer: 'Jumpman'
+    },
+    {
+        id: 18,
+        question: 'Which combination of keyboard shortcut keys are to open Windows Task Manager?',
+        options: ['Ctrl + Shift + Esc','Ctrl + Alt + Del','Ctrl + Alt + F3'],
+        correctAnswer: 'Ctrl + Shift + Esc'
+    },
+    {
+        id: 19,
+        question: 'What is the name of the first ever video game that was released in the year 1972?',
+        options: ['Super Mario','Pong','Solitaire'],
+        correctAnswer: 'Pong'
+    },
+    {
+        id: 20,
+        question: 'What does the "M" in MP3 stand for?',
+        options: ['Movie','Music','Moving'],
+        correctAnswer: 'Moving'
+    }
+];
