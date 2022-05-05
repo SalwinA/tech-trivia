55b4da096dd18f522efa42a8c4a48f9eb14d2dad
SalwinA <salwino@gmail.com>
2022-05-05 13:47:40 +0000

    Add basic structure to quiz page.

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let q1 = document.getElementById('question-space');
+    q1 = question_bank[1];
+    let op1 = document.getElementById('option1');
+    let op2 = document.getElementById('option2');
+    let op3 = document.getElementById('option3');
+})
