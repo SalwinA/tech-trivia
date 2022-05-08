8d628b6fc337c725d5bece3699721dc4cd577e36
SalwinA <salwino@gmail.com>
2022-05-08 19:10:20 +0000

    Add question code for quiz page.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,6 @@ const questionBank =[
     {
         id: 1,
         question: 'The Firefox browser logo is not a fox? It is actually a type of species which are protected in Asia.',
-        // options: ['Slow Loris','Red Panda','Giant Panda'],
         option1: 'Slow Loris',
         option2: 'Red Panda',
         option3: 'Giant Panda',
@@ -19,110 +18,146 @@ const questionBank =[
     {
         id: 3,
         question: 'The average computer user blinks only _____ times per minute, while the normal rate is 20 times per minute.',
-        options: ['7','2','10'],
-        correctAnswer: '7'
+        option1: '7',
+        option2: '2',
+        option3: '10',
+        correctAnswer: 'option1'
     },
     {
         id: 4,
         question: 'The “Google” name was created mistakenly. The original name was supposedly.,',
-        options: ['Googel','Googolplex','Googol'],
-        correctAnswer: 'Googol'
+        option1: 'Googel',
+        option2: 'Googolplex',
+        option3: 'Googol',
+        correctAnswer: 'option3'
     },
     {
         id: 5,
-        question: 'This font from Microsoft was a controversial font after the 9/11 attacks in 2001 where the first plane was first to hit.',
-        options: ['Arial Narrow','Lucida Console','Wingdings'],
-        correctAnswer: 'Wingdings'
+        question: 'This font from Microsoft was a controversial font after the 9/11 attacks in 2001 where the first plane was first to hit.',        
+        option1: 'Arial Narrow',
+        option2: 'Lucida Console',
+        option3: 'Wingdings',
+        correctAnswer: 'option3'
     },
     {
         id: 6,
-        question: 'The first VCR or Video Camera Recorder in the year 1956 was the size of a:',
-        options: ['Piano','Television','Radio'],
-        correctAnswer: 'Piano'
+        question: 'The first VCR or Video Camera Recorder in the year 1956 was the size of a:',        
+        option1: 'Piano',
+        option2: 'Television',
+        option3: 'Radio',
+        correctAnswer: 'option1'
     },
     {
         id: 7,
-        question: 'That _____ of people try to plug their USB devices upside down?',
-        options: ['86%','50%','30%'],
-        correctAnswer: '86%'
+        question: 'That _____ of people try to plug their USB devices upside down?',        
+        option1: '86%',
+        option2: '50%',
+        option3: '30%',
+        correctAnswer: 'option1'
     },
     {
         id: 8,
-        question: 'The first American alarm clock that was invented could only ring at ____?',
-        options: ['4am','6am','12pm'],
-        correctAnswer: '4am'
+        question: 'The first American alarm clock that was invented could only ring at ____?',        
+        option1: '4am',
+        option2: '6am',
+        option3: '12pm',
+        correctAnswer: 'option1'
     },
     {
         id: 9,
-        question: 'The first world computer mouse was made of:',
-        options: ['Metal','Wood','Chrome'],
-        correctAnswer: 'Wood'
+        question: 'The first world computer mouse was made of:',        
+        option1: 'Metal',
+        option2: 'Wood',
+        option3: 'Chrome',
+        correctAnswer: 'option2'
     },
     {
         id: 10,
         question: 'The Astronauts of Apollo 11 couldn’t get life insurance because it cost a fortune? Instead, they signed:',
-        options: ['Autographs','Waiver','Farewell Letter'],
-        correctAnswer: 'Autographs'
+        option1: 'Autographs',
+        option2: 'Waiver',
+        option3: 'Farewell Letter',
+        correctAnswer: 'option1'
     },
     {
         id: 11,
         question: 'What was the app Snapchat originally named when launched in 2011?',
-        options: ['Facesnap','Picaboo','Chatsnap'],
-        correctAnswer: 'Picaboo'
+        option1: 'Facesnap',
+        option2: 'Picaboo',
+        option3: 'Chatsnap',
+        correctAnswer: 'option2'
     },
     {
         id: 12,
         question: '_____ of people type their word or sentence on google, just to see if they spelled it right?',
-        options: ['10%','50%','97%'],
-        correctAnswer: '97%'
+        option1: '10%',
+        option2: '50%',
+        option3: '97%',
+        correctAnswer: 'option3'
     },
     {
         id: 13,
         question: 'What does the word “Android” mean?',
-        options: ['A green robot.','A robot with human appearance.','An articulated robot.'],
-        correctAnswer: 'A robot with human appearance.'
+        option1: 'A green robot.',
+        option2: 'A robot with human appearance.',
+        option3: 'An articulated robot.',
+        correctAnswer: 'option2'
     },
     {
         id: 14,
         question: 'What is the most used password despite all warnings is?',
-        options: ['123456','abcdef','abc123'],
-        correctAnswer: '123456'
+        option1: '123456',
+        option2: 'abcdef',
+        option3: 'abc123',
+        correctAnswer: 'option1'
     },
     {
         id: 15,
         question: 'Name the syndrome in which one thinks that their phone is vibrating but isn’t?',
-        options: ['Forced Vibration','Phantom Vibration','Damped Vibration'],
-        correctAnswer: 'Phantom Vibration'
+        option1: 'Forced Vibration',
+        option2: 'Phantom Vibration',
+        option3: 'Damped Vibration',
+        correctAnswer: 'option2'
     },
     {
         id: 16,
         question: 'Fear of technology is called ________.',
-        options: ['Glossophobia','Tripophobia','Technophobia'],
-        correctAnswer: 'Technophobia'
+        option1: 'Glossophobia',
+        option2: 'Tripophobia',
+        option3: 'Technophobia',
+        correctAnswer: 'option3'
     },
     {
         id: 17,
         question: 'What is the first name of the famous character in video game Super Mario?',
-        options: ['Jumperman','Superjump','Jumpman'],
-        correctAnswer: 'Jumpman'
+        option1: 'Jumperman',
+        option2: 'Superjump',
+        option3: 'Jumpman',
+        correctAnswer: 'option3'
     },
     {
         id: 18,
         question: 'Which combination of keyboard shortcut keys are to open Windows Task Manager?',
-        options: ['Ctrl + Shift + Esc','Ctrl + Alt + Del','Ctrl + Alt + F3'],
-        correctAnswer: 'Ctrl + Shift + Esc'
+        option1: 'Ctrl + Shift + Esc',
+        option2: 'Ctrl + Alt + Del',
+        option3: 'Ctrl + Alt + F3',
+        correctAnswer: 'option1'
     },
     {
         id: 19,
         question: 'What is the name of the first ever video game that was released in the year 1972?',
-        options: ['Super Mario','Pong','Solitaire'],
-        correctAnswer: 'Pong'
+        option1: 'Super Mario',
+        option2: 'Pong',
+        option3: 'Solitaire',
+        correctAnswer: 'option2'
     },
     {
         id: 20,
         question: 'What does the "M" in MP3 stand for?',
-        options: ['Movie','Music','Moving'],
-        correctAnswer: 'Moving'
+        option1: 'Movie',
+        option2: 'Music',
+        option3: 'Moving',
+        correctAnswer: 'option3'
     }
 ];
 
@@ -134,8 +169,9 @@ const submit = document.querySelector('#submit');
 
 const answers = document.querySelectorAll('.answer');
 
-let questionCount = 0;
+let questionCount = (Math.floor(Math.random()*10));
 let score = 0;
+const showScore = document.querySelector('#showScore');
 
 const currentQuestion = questionBank[questionCount];
 
@@ -158,7 +194,7 @@ const getAnswer = () => {
             
         });
         return answer;
-}
+};
 
 submit.addEventListener('click', () => {
     const selctdAnswer = getAnswer();
@@ -166,13 +202,15 @@ submit.addEventListener('click', () => {
 
     if(selctdAnswer === questionBank[questionCount].correctAnswer){
         score++;
-    }
-    console.log(score);
+    };
     questionCount++;
-
-    if(questionCount < 10) {
+    if(questionCount < 5){
         questionLoad();
     }
+        showScore.innerHTML = `
+        <h3> Your Score is ${score} </h3>
+        `
+    
 
 });
 
